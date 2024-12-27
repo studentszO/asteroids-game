23a7868463e61b8a973eaa3eeb02ac58deef0ede
studentszO <169681673+studentszO@users.noreply.github.com>
2024-12-27 11:02:35 +0100

    fix: asteroids now split in two

--- asteroid.py
+++ asteroid.py
@@ -1,5 +1,7 @@
 import pygame
+import random
 from circleshape import CircleShape
+from constants import *
 
 class Asteroid(CircleShape):
     def __init__(self, x, y, radius):
@@ -10,4 +12,19 @@ class Asteroid(CircleShape):
     
     def update(self, dt):
         self.position += self.velocity * dt
+    
+    def split(self):
+        self.kill()
+        if self.radius < ASTEROID_MIN_RADIUS:
+            return
+        else:
+            random_angle = random.uniform(20.0, 50.0)
+            new_asteroid_vector = self.velocity.rotate(random_angle)
+            new_asteroid_vector_two = self.velocity.rotate(-random_angle)
+            new_radius = self.radius - ASTEROID_MIN_RADIUS
+            new_asteroid = Asteroid(self.position[0], self.position[1], new_radius)
+            second_new_asteroid = Asteroid(self.position[0], self.position[1], new_radius)
+            new_asteroid.velocity = new_asteroid_vector_two * 1.2
+            second_new_asteroid.velocity = new_asteroid_vector * 1.2
+
     
--- main.py
+++ main.py
@@ -53,7 +53,7 @@ def main():
                 sys.exit()
             for shot in shots_group:
                 if asteroid.check_collision(shot):
-                    asteroid.kill()
+                    asteroid.split()
                     shot.kill()
 
         dt = clock.tick(60) / 1000
